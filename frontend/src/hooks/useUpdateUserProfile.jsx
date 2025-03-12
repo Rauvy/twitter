b4778a0e895f2e902ae0b1127c0c779b7b4361eb
Rehina Yakubiv <yakubiv@Rehinas-MacBook-Pro.local>
2025-03-12 16:22:13 -0400

    Added backend links

--- frontend/src/hooks/useUpdateUserProfile.jsx
+++ frontend/src/hooks/useUpdateUserProfile.jsx
@@ -7,7 +7,7 @@ const useUpdateUserProfile = () => {
 	const { mutateAsync: updateProfile, isPending: isUpdatingProfile } = useMutation({
 		mutationFn: async (formData) => {
 			try {
-				const res = await fetch(`/api/users/update`, {
+				const res = await fetch(`https://twitter-euod.onrender.com/api/users/update`, {
 					method: "POST",
 					headers: {
 						"Content-Type": "application/json",
--- frontend/src/pages/auth/login/LoginPage.jsx
+++ frontend/src/pages/auth/login/LoginPage.jsx
@@ -23,7 +23,7 @@ const LoginPage = () => {
 	} = useMutation({
 		mutationFn: async ({ username, password }) => {
 			try {
-				const res = await fetch("/api/auth/login", {
+				const res = await fetch("https://twitter-euod.onrender.com/api/auth/login", {
 					method: "POST",
 					headers: {
 						"Content-Type": "application/json",
